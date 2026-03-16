a9aa4bb38bb26192aa216b26800430b2fffa27b3
ckendrick-mqu <christopher.kendrick1@students.mq.edu.au>
2026-03-17 09:01:14 +1100

    remove old animation logic

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-            }
-        });
-    }, { 
-    });
-
-    const hiddenElements = document.querySelectorAll('.fade-in');
-    hiddenElements.forEach((el) => observer.observe(el));
-});
-
-document.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
